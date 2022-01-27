adab982def669c31357d61fa20c4328f6bbafb64
Santiago Buitrago <brayan.buitrago@escuelaing.edu.co>
2022-01-27 10:54:20 -0500

    Laboratorio 1

---
+++ src/test/java/co/edu/eci/ieti/laboratorio_1/Laboratorio1ApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.eci.ieti.laboratorio_1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Laboratorio1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
